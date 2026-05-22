2def44e2d516e775c2c076e3d9bfc2fd1ef9ab52
Zeal Shah <zealshah95@gmail.com>
2026-05-21 17:33:54 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "zeal"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
