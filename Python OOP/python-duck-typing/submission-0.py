6bcfa5b56a35d7b1e412c3af8b3ba61490177dbb
Zeal Shah <zealshah95@gmail.com>
2026-05-21 19:01:57 -0700

    Add: python-duck-typing - submission-0

---
+++ Python OOP/python-duck-typing/submission-0.py
@@ -0,0 +1,27 @@
+class SpiderMan:
+    def attack(self) -> str:
+        return "Web Shooter!"
+    
+    def defend(self) -> str:
+        return "Spider Sense!"
+
+# TODO: Create the BlackWidow class with attack() and defend() methods
+class BlackWidow:
+    def attack(self) -> str:
+        return "Widow's Bite!"
+    
+    def defend(self) -> str:
+        return "Acrobatic Dodge!"
+
+# TODO: Create the battle_sequence() function
+def battle_sequence(hero):
+    print(f"{hero.attack()}")
+    print(f"{hero.defend()}")
+
+
+# Don't modify the code below
+spider_man = SpiderMan()
+black_widow = BlackWidow()
+
+battle_sequence(spider_man)
+battle_sequence(black_widow)
