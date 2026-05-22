074bf3e7a69717fd3d54a7873dc0c6a37c716d52
Zeal Shah <zealshah95@gmail.com>
2026-05-22 14:11:21 -0700

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,11 @@
+def add_two_numbers() -> int:
+    ip = input()
+    a, b = int(ip.split(",")[0]), int(ip.split(",")[1]) 
+    return a + b
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
